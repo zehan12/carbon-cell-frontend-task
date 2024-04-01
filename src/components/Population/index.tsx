c0dc467ccb82d8ad185a1b4eb25d2f961e11715d
Zehan Khan <zkhan@hootboard.com>
2024-04-01 06:11:11 +0530

    feat: :chart_with_upwards_trend: add population data chart

--- src/components/Population/index.tsx
+++ src/components/Population/index.tsx
@@ -1,15 +1,90 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { fetchPopulationData } from "../../services/api";
+import ReactApexChart from "react-apexcharts";
 
 const Population: FC = () => {
+  const [data, setData] = useState([]);
+  const [chartData, setChartData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
   useEffect(() => {
     const fetchPopulationGraphData = async () => {
       const data = await fetchPopulationData();
       console.log(data);
+      return setData(data);
     };
     fetchPopulationGraphData();
   }, []);
-  return <></>;
+
+  useEffect(() => {
+    setLoading(true);
+    if (data) {
+      const formattedData = data.map((entry: any) => ({
+        x: entry.Year,
+        y: entry.Population,
+      }));
+      setChartData(formattedData);
+      setLoading(false);
+    }
+  }, [data]);
+
+  const chartOptions = {
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    title: {
+      text: "Population Trend Over Years",
+      align: "left",
+      style: {
+        color: "#ffffff",
+      },
+    },
+    xaxis: {
+      type: "category",
+      labels: {
+        style: {
+          colors: "#ffffff",
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: "#ffffff",
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="bg-zinc-800 h-screen overflow-hidden">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ReactApexChart
+          options={chartOptions}
+          series={[
+            {
+              name: "Population",
+              data: chartData,
+            },
+          ]}
+          type="line"
+          height={600}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Population;
